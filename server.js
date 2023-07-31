23efd1bce014879ffca1b63602b40642e104d858
Justin Duong <jduong@umich.edu>
2023-07-31 18:52:21 -0400

    Successfully connected fly.io with mongo database, user can add message to database.

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = 3000;
+
+const username = encodeURIComponent(process.env.username);
+const password = encodeURIComponent(process.env.password);
+const uri = `mongodb+srv://${username}:${password}@cluster0.09zsxgi.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 const messages = [
   {
@@ -14,7 +28,28 @@ const messages = [
     user: "Charles",
     added: new Date()
   }
-]
+];
+
+async function run() {
+  await client.connect();
+
+  const dbName = "miniMessageBoard";
+  const collectionName = "messages";
+
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  try {
+    await collection.insertMany(messages);
+  } catch (err) {
+    console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.log.dir);
+
+
 
 app.set('view engine', 'pug');
 app.use(express.urlencoded({ extended: true }))
@@ -27,12 +62,19 @@ app.get('/new', (req, res) => {
   res.render('form', { title: 'New Message', headerOne: 'New Message' });
 });
 
-app.post('/new', (req, res) => {
-  messages.push({ 
+app.post('/new', async (req, res) => {
+  const userData = { 
     text: req.body.message,
     user: req.body.user,
     added: new Date()
-  });
+  };
+
+  await client.connect();
+  await client.db('miniMessageBoard').collection('messages').insertOne(userData, (err, res) => {
+    if (err) throw err;
+    console.log("1 message inserted");
+    client.close();
+  })
 
   res.redirect('/');
 });
